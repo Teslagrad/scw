c70ae5971732729322125e7c1c751589d9611ef5
Teslagrad <941180029@qq.com>
2020-03-18 17:48:10 +0800

    完成项目发布的第一个表单提交

---
+++ scw-project/src/main/java/com/atguigu/scw/project/component/OssTemplate.java
@@ -0,0 +1,49 @@
+package com.atguigu.scw.project.component;
+
+import java.io.InputStream;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@ToString
+@Data
+@Slf4j
+public class OssTemplate {
+
+	// Endpoint以杭州为例，其它Region请按实际情况填写。
+	String endpoint;
+	// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录
+	// https://ram.console.aliyun.com 创建。
+	String accessKeyId;
+	String accessKeySecret;
+	String bucket;
+
+	public String upload(String filename, InputStream inputStream) {
+		try {
+
+			// 创建OSSClient实例。
+			OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+			// 上传文件流。
+			// InputStream inputStream = new FileInputStream("F:/temp/p1.jpg");
+			ossClient.putObject(bucket, "pic/" + filename, inputStream);
+
+			// 关闭OSSClient。
+			ossClient.shutdown();
+
+			String filePath = "https://" + bucket + "." + endpoint + "/pic/" + filename;
+			log.debug("文件上传成功-{}", filePath);
+			// 返回文件名
+			return filePath;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			log.debug("文件上传失败-{}", filename);
+			return null;
+		}
+	}
+}
--- scw-project/src/main/java/com/atguigu/scw/project/controller/ProjectCreateController.java
+++ scw-project/src/main/java/com/atguigu/scw/project/controller/ProjectCreateController.java
@@ -1,23 +1,92 @@
 package com.atguigu.scw.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.scw.project.component.OssTemplate;
+import com.atguigu.scw.project.constant.ProjectConstant;
+import com.atguigu.scw.project.vo.req.BaseVo;
+import com.atguigu.scw.project.vo.req.ProjectRedisStorageVo;
 import com.atguigu.scw.vo.resp.AppResponse;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @author Tesla
+ *
+ *         2020年3月13日 下午5:39:36
+ */
+@Slf4j
 @Api(tags = "项目发起模块")
 @RequestMapping("/project/create")
 @RestController
 public class ProjectCreateController {
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	OssTemplate ossTemplate;
 
 	@ApiOperation(value = "1-项目初始创建")
 	@PostMapping("/init")
-	public AppResponse<Object> init() {
-		return AppResponse.ok("ok");
+	public AppResponse<Object> init(BaseVo vo) {
+
+		try {
+			// 判断用户token是否为空
+			String accessToken = vo.getAccessToken();
+			if (StringUtils.isEmpty(accessToken)) {
+				AppResponse resp = AppResponse.fail(null);
+				resp.setMsg("请求必须提速accessToken");
+				return resp;
+			}
+
+			String memberId = stringRedisTemplate.opsForValue().get(accessToken);
+
+			// 判断用户token是否存在
+			if (StringUtils.isEmpty(memberId)) {
+				AppResponse resp = AppResponse.fail(null);
+				resp.setMsg("请先登录");
+				return resp;
+			}
+
+			// 小vo对拷到大vo
+			ProjectRedisStorageVo bigVo = new ProjectRedisStorageVo();
+
+			BeanUtils.copyProperties(vo, bigVo);
+
+			// 生成代表项目的token作为key，大vojson作为值
+			String projectToken = UUID.randomUUID().toString().replace("-", "");
+
+			bigVo.setProjectToken(projectToken);
+
+			// vo转为json
+			String bigstr = JSON.toJSONString(bigVo);// fastjson
+
+			stringRedisTemplate.opsForValue().set(ProjectConstant.TEMP_PROJECT_PREFIX + projectToken, bigstr);
+
+			log.debug("大vo数据：{}", bigVo);
+
+			return AppResponse.ok(bigVo);// appresonse组件属于jackson组件,可以把apprespone对象转换成json
+		} catch (BeansException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return AppResponse.fail(null);
+		}
 	}
 
 	@ApiOperation(value = "2-项目基本信息")
@@ -38,10 +107,32 @@ public class ProjectCreateController {
 		return AppResponse.ok("ok");
 	}
 
+	/**
+	 * 文件上传表单要求： ①method="post" ②enctype="multipart/form-data" ③type="file"
+	 * 
+	 * SpringMVC框架集成commons-fileupload和commons-io组件，完成文件上传操作。 SpringMVC提供文件上传解析器。
+	 * Controller处理文件上传时，通过MultipartFile接受文件。
+	 */
 	@ApiOperation(value = "上传图片")
 	@PostMapping("/upload")
-	public AppResponse<Object> upload() {
-		return AppResponse.ok("ok");
+	public AppResponse<Object> upload(@RequestParam("uploadfile") MultipartFile[] files) {
+
+		try {
+			List<String> filepathList = new ArrayList<String>();
+
+			for (MultipartFile multipartFile : files) {
+				String filename = multipartFile.getOriginalFilename();
+				filename = UUID.randomUUID().toString().replaceAll("-", "") + "_" + filename;// 拼串，保证图片文件名不会重复
+				String filepath = ossTemplate.upload(filename, multipartFile.getInputStream());
+				filepathList.add(filepath);
+			}
+			log.debug("上传文件的路径-{}", filepathList);
+			return AppResponse.ok(filepathList);// 返回所有图片信息，可以放到隐藏域里
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("上传文件出现异常");
+			return AppResponse.fail(null);
+		}
 	}
 
 //	@ApiOperation(value = "4--确认项目法人信息")
